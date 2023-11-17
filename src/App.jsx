7a82f92ad9df547dafa9dd0e2b2b3102c9ebec8e
Zagaz <andre_ranulfo@hotmail.com>
2023-11-17 15:46:05 -0300

    Use State

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-
-import Captains from './components/Captains'
-import { useState } from 'react'
-
+import React from 'react'
+import UseState01 from './components/UseState01'
 
 export default function App() {
-  const [value, setValue] = useState(0)
-
-  function increment() {
-
-    setValue(old => old + 1)
-
-  }
-  function decrement() {
-    setValue(old => old - 1)
-  }
-  function clear() {
-    setValue(old => old = 0)
-
-  }
 
   return (
-
     <>
-      <h1>Value: {value}</h1>
-      <button onClick={increment}>ADD</button>
-      <button onClick={decrement}>SUB</button>
-      <button onClick={clear}>CLEAR</button>
-
-      <br />
-      <br />
-      <hr />
+       <UseState01 />
     </>
   )
+ 
+
+
 
 
 }
