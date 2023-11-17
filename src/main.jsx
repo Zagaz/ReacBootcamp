6da47ca503e103455839b75b0c0106c4aebe0f71
Zagaz <andre_ranulfo@hotmail.com>
2023-11-17 13:32:17 -0300

    Props 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Button from './components/Button'
-import Table from './components/Table'
-import Image from './components/views/Image'
+// import Button from './components/Button'
+// import Table from './components/Table'
+// import Image from './components/views/Image'
+import Article from './components/Article'
 import Data from './components/Data'
 import './App.css'
 
@@ -12,5 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Data />
+    
+    <Article title = "Lorem Firstum" article = "Firstum ut mollit sint fugiat excepteur elit enim dolor sint mollit excepteur irure eu."/>
+    <Article title = "Lorem Secundum" article = "Lorem Secundum velit ad velit commodo adipisicing quis nisi culpa voluptate et dolore enim. ."/>
+ 
   </React.StrictMode>,
 )
