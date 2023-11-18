f4002eb2a7f60dd346d2039abdcfea6eca571c28
Zagaz <andre_ranulfo@hotmail.com>
2023-11-18 17:40:30 -0300

    UseState5

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import UseState01 from './components/UseState01'
 import UseState02 from './components/UseState02'
 import UseState03 from './components/UseState03'
 import UseState04 from './components/UseState04'
+import UseState05 from './components/UseState05'
 
 export default function App() {
-
   return (
     <>
-       {/* <UseState01 /> */}
-       {/* <UseState02 /> */}
-       {/* <UseState03 /> */}
-       <UseState04 />
-       
+    {/* Simple useState add/sub/reset */}
+      <UseState01 />
+      {/* useState for 2 different variables */}
+      <UseState02 />
+      {/* Same as above, but calculating the hypotenuse of a triangle */}
+      <UseState03 />
+      {/* Usinh map to iterate*/}
+      <UseState04 />
+      {/* Using a button component to change state on parent | passing a function as a property */}
+       <UseState05 />    
     </>
   )
- 
-
-
-
-
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import Button from './components/Button'
-// import Table from './components/Table'
-// import Image from './components/views/Image'
-
-//import Data from './components/Data'
-import './App.css'
-import Caps from './components/Caps'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Caps />
-
-
-
-
   </React.StrictMode>,
 )
