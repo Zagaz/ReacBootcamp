9e420c4066f1dfd9cc3d42347d227eae4536fdf6
Zagaz <andre_ranulfo@hotmail.com>
2023-11-19 18:06:06 -0300

    UseState06 - END

--- src/components/UseState06.jsx
+++ src/components/UseState06.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import Cathetus from "./Cathetus";
-import "./UseState05.css";
+ import "./UseState06.css";
+
 
 // exercise 4
 // DISCLAIMER
